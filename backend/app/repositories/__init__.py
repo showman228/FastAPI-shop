ac0e59b1d3699510c376dc78ca9852af7a053fc2
showman228 <nekrasovaleksey.1999@gmail.com>
2026-01-12 11:54:12 +0300

    bug fix about import

---
+++ backend/app/repositories/__init__.py
@@ -0,0 +1,7 @@
+from ..repositories.category_repositories import CategoryRepository
+from ..repositories.product_repositories import  ProductRepositories
+
+__all__ = [
+    "CategoryRepository",
+    "ProductRepositories",
+]
