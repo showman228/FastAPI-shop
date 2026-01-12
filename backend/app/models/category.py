161a6dafa4b5b2b60949e4cd0a857764806a4cd6
showman228 <nekrasovaleksey.1999@gmail.com>
2026-01-12 12:46:20 +0300

    fix bugs and add main, run files

--- backend/app/models/category.py
+++ backend/app/models/category.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import relationship
 from ..database import Base
 
 class Category(Base):
-    __tablename__ = "category"
+    __tablename__ = "categories"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False, unique=True, index=True)
